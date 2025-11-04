77b65731fb4f086e46a4f6f5ef18fbf3e5ffac35
Arvind H S <arvindbond52@gmail.com>
2025-11-04 11:18:25 +0530

    Update CORS allowed origins in Configurationa.java

--- src/main/java/com/crud/email/config/Configurationa.java
+++ src/main/java/com/crud/email/config/Configurationa.java
@@ -13,7 +13,7 @@ public class Configurationa {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOrigins("https://emailscheduler-production-c3d0.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
